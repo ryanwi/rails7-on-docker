a51dc8db9a249f0b9da06c9c30cb089264c4df2d
Ryan W <rwilliamz@gmail.com>
2021-09-26 14:41:38 -0700

    Add all app files

---
+++ config/importmap.rb
@@ -0,0 +1,10 @@
+# Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
+# pin "@rails/activestorage", to: "activestorage.esm.js"
+
+# Use node modules from a JavaScript CDN by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.js"
+pin "@hotwired/stimulus", to: "stimulus.js"
+pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
