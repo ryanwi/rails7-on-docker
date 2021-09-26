f01c8842afab201072c8aa6e67a95ba15d9f30d6
Ryan W <rwilliamz@gmail.com>
2021-09-26 15:48:25 -0700

    back to original order

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,8 +3,8 @@
 
 # Use node modules from a JavaScript CDN by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/stimulus", to: "stimulus.js"
-pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@hotwired/turbo-rails", to: "turbo.js"
+pin 'application'
+pin '@hotwired/turbo-rails', to: 'turbo.js'
+pin '@hotwired/stimulus', to: 'stimulus.js'
+pin '@hotwired/stimulus-importmap-autoloader', to: 'stimulus-importmap-autoloader.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
