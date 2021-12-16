826b228df81ebd29ca9c2693ce1a36874bff31f5
Ryan W <rwilliamz@gmail.com>
2021-12-16 10:50:28 -0800

    Add action-text

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "trix"
+import "@rails/actiontext"
 
 console.log('Hello World from application.js');
