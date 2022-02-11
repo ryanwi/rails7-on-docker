100521b48f44f3050de4526494cd232b9eaba73c
Ryan W <rwilliamz@gmail.com>
2022-02-11 12:14:36 -0800

    cleanup

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ require 'action_cable/engine'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "propshaft"
 
 module Railsondocker
   class Application < Rails::Application
