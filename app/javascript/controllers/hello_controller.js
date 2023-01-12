96f323a88c145bcd1ccfa38733c2ce93bebb5cf7
Ryan W <rwilliamz@gmail.com>
2023-01-11 22:19:23 -0800

    more stimulus tests

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+ static targets = [ "name", "output" ]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!"
+    console.log('Hello World hello_controller.js');
+  }
+
+  greet() {
+    console.log('greet');
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
   }
 }
