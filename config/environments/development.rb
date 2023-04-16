f7b678a17adb1581bece08b636499a0b387fd176
Ryan W <rwilliamz@gmail.com>
2023-04-16 09:48:47 -0700

    config updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
