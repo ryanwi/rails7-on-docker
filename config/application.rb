0946324eaf693cb58c5bbc1b53caa5cfc09cd424
Ryan W <rwilliamz@gmail.com>
2023-06-01 10:01:19 -0700

    update defaults

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ Bundler.require(*Rails.groups)
 module Railsondocker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Configuration for the application, engines, and railties goes here.
     #
