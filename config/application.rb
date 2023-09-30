e4c632e97657005d51bad4b670c241da4eac0c83
Ryan W <rwilliamz@gmail.com>
2023-09-30 09:40:21 -0700

    latest app:update

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,9 @@ module Railsondocker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, see https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#config-autoload-lib-ignore.
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
