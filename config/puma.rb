85a98452424ff20b2a1a0d9b128ab7ce74750e49
Ryan W <rwilliamz@gmail.com>
2023-09-30 14:31:45 -0700

    Back to config

--- config/puma.rb
+++ config/puma.rb
@@ -25,8 +25,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-# port ENV.fetch("PORT") { 3000 }
-port 3000
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
