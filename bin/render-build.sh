36054a37d32fd92af4f531808e4a14f273379b64
Ryan W <rwilliamz@gmail.com>
2023-09-30 21:27:33 -0700

    Trying Render buildCommand

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+echo "render build"
+
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
