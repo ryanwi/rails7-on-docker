10242be95c783e0f648dab56c5dca67bf6e1ae9d
Ryan W <rwilliamz@gmail.com>
2024-02-04 10:44:42 -0800

    style: Rubocop corrections

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,5 +9,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 
-pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "popper", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
